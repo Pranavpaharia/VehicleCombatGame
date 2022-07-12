68a61cae3dd21c570c78e3230cd550ce1eeaefdc
pranavsharechat <pranavpaharia@sharechat.co>
2022-07-13 00:09:23 +0530

    Added required enums

--- Source/VehicleUE5/VehicleUE5.h
+++ Source/VehicleUE5/VehicleUE5.h
@@ -3,3 +3,29 @@
 #pragma once
 
 #include "CoreMinimal.h"
+
+
+UENUM(BlueprintType)
+enum class VehicleBasicAbilityID : uint8
+{
+	Alive,
+	Dead
+};
+
+UENUM(BlueprintType)
+enum class VehiclePowerAbilityID : uint8
+{
+	FireBullets,
+	FireRockets,
+	Mines,
+	Shield
+};
+
+
+UENUM(BlueprintType)
+enum class ETypesOfVehicleID : uint8
+{
+	Vehicle01,
+	Vehicle02,
+	Vehicle03
+};
