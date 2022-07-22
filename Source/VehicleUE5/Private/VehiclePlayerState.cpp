f1b494f52b0341518578cbfa11a7facbe5dbd07d
pranavsharechat <pranavpaharia@sharechat.co>
2022-07-22 06:41:33 +0530

    Added Ability component and attributeset initialization Added Set Session ID method, Setters and Getters for the Health/Nitro Added OnMana/OnHealth changed handlers and virtual functions

--- Source/VehicleUE5/Private/VehiclePlayerState.cpp
+++ Source/VehicleUE5/Private/VehiclePlayerState.cpp
@@ -4,6 +4,8 @@
 #include "VehiclePlayerState.h"
 #include "Net/OnlineEngineInterface.h"
 #include "Net/UnrealNetwork.h"
+#include "VehicleUE5Pawn.h"
+#include "Widgets/VehicleBaseInfoWidget.h"
 #include "VehiclePlayerController.h"
 
 AVehiclePlayerState::AVehiclePlayerState()
@@ -11,7 +13,61 @@ AVehiclePlayerState::AVehiclePlayerState()
 	ColorId = 1;
 	CandidateName = TEXT("");
 	TextChat = TEXT("-.^");
+	sessionStringID = TEXT("");
+
+	AbilitySystemComponent = CreateDefaultSubobject<UVehicleAbilitySystemComponent>(TEXT("AbilitySystemComponent"));
+	AbilitySystemComponent->SetIsReplicated(true);
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Mixed);
+
+	AttributeSetBase = CreateDefaultSubobject<UVehicleAttributeSet>(TEXT("AttributeSetBase"));
+
+	NetUpdateFrequency = 100;
+	
+	DeadTag = FGameplayTag::RequestGameplayTag((FName("State.Dead")));
 }
+
+void AVehiclePlayerState::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (AbilitySystemComponent)
+	{
+		HealthChangedDelegateHandle = AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(AttributeSetBase->GetHealthAttribute()).AddUObject(this, &AVehiclePlayerState::OnHealthChanged);
+		ManaChangedDelegateHandle = AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(AttributeSetBase->GetNitroManaAttribute()).AddUObject(this, &AVehiclePlayerState::OnManaChanged);
+		MaxHealthChangedDelegateHandle = AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(AttributeSetBase->GetMaxHealthAttribute()).AddUObject(this, &AVehiclePlayerState::OnMaxHealthChanged);
+		ManaChangedDelegateHandle = AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(AttributeSetBase->GetNitroManaAttribute()).AddUObject(this, &AVehiclePlayerState::OnMaxManaChanged);
+
+		AbilitySystemComponent->RegisterGameplayTagEvent(FGameplayTag::RequestGameplayTag(FName("State.DamageHit")), EGameplayTagEventType::NewOrRemoved).AddUObject(this, &AVehiclePlayerState::OnVehicleDamageHit);
+	}
+	
+	
+}
+
+UAbilitySystemComponent* AVehiclePlayerState::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
+}
+
+UVehicleAttributeSet* AVehiclePlayerState::GetAttributeSetBase() const
+{
+	return AttributeSetBase;
+}
+
+bool AVehiclePlayerState::IsAlive() const
+{
+	return true;
+}
+
+void AVehiclePlayerState::ShowAbilityConfirmCancelText(bool ShowText)
+{
+	AVehiclePlayerController* PC = Cast<AVehiclePlayerController>(GetOwner());
+	if (PC)
+	{
+
+	}
+}
+
+
 
 void AVehiclePlayerState::PostInitializeComponents()
 {
@@ -36,7 +92,7 @@ void AVehiclePlayerState::ClientInitialize(class AController* InController)
 	AVehiclePlayerController* pc = Cast<AVehiclePlayerController>(InController);
 	if (pc)
 	{
-		UE_LOG(LogTemp, Warning, TEXT("Controller broadcast player state"));
+		UE_LOG(LogTemp, Warning, TEXT("AVehiclePlayerState::ClientInitialize"));
 		pc->OnNewPlayerStateReceived.Broadcast(this);
 	}
 
@@ -47,6 +103,10 @@ void AVehiclePlayerState::RegisterPlayerWithSession(bool bWasFromInvite)
 	if (UOnlineEngineInterface::Get()->DoesSessionExist(GetWorld(),NAME_GameSession))
 	{
 		Super::RegisterPlayerWithSession(bWasFromInvite);
+		UEnum* EnumPtr = FindObject<UEnum>(ANY_PACKAGE, TEXT("GameSession"));
+
+
+		UE_LOG(LogTemp, Warning, TEXT("Register Player with Session %s"), *(EnumPtr ? EnumPtr->GetNameStringByIndex(static_cast<uint8>(NAME_GameSession)) : TEXT("Invalid Enum")));
 	}
 }
 
@@ -76,6 +136,7 @@ void AVehiclePlayerState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& 
 	DOREPLIFETIME(AVehiclePlayerState, ColorId);
 	DOREPLIFETIME(AVehiclePlayerState, CandidateName);
 	DOREPLIFETIME(AVehiclePlayerState, TextChat);
+	DOREPLIFETIME(AVehiclePlayerState, sessionStringID);
 
 }
 
@@ -92,6 +153,12 @@ void AVehiclePlayerState::OnRep_CandidateName()
 	OnCandidateNameChanged.Broadcast(CandidateName);
 }
 
+void AVehiclePlayerState::OnRep_sessionStringID()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Session ID replicated:  %s"), *sessionStringID);
+	OnSetSessionStringEvent.Broadcast(sessionStringID);
+}
+
 void AVehiclePlayerState::SetPlayerNickName(FString Name)
 {
 	CandidateName = Name;
@@ -101,3 +168,119 @@ void AVehiclePlayerState::UpdateChatMessage(FString Msg)
 {
 	TextChat = Msg;
 }
+
+void AVehiclePlayerState::SetSessionIDString(FString str)
+{
+	sessionStringID = str;
+}
+
+float AVehiclePlayerState::GetHealth() const
+{
+	return AttributeSetBase->GetHealth();
+}
+
+float AVehiclePlayerState::GetMaxHealth() const
+{
+	return  AttributeSetBase->GetMaxHealth();
+}
+
+float AVehiclePlayerState::GetMana() const
+{
+	return AttributeSetBase->GetNitroMana();
+}
+
+float AVehiclePlayerState::GetMaxMana() const
+{
+	return AttributeSetBase->GetNitroMaxMana();
+}
+
+void AVehiclePlayerState::OnHealthChanged(const FOnAttributeChangeData& Data)
+{
+	float Health = Data.NewValue;
+
+	//Set Health on vehicle Pawn and UI Widgets
+	AVehicleUE5Pawn* VehiclePawn = Cast<AVehicleUE5Pawn>(GetPawn());
+	if (VehiclePawn && GetLocalRole() != ROLE_Authority)
+	{
+		UVehicleBaseInfoWidget* PlayerInfoWidget = VehiclePawn->GetFloatingStatusBar();
+		if (PlayerInfoWidget)
+		{
+			PlayerInfoWidget->SetHealthBar(Health / GetMaxHealth());
+		}
+	}
+
+	UE_LOG(LogTemp, Warning, TEXT("On Player State: On Health Changed to %f"), Health);
+
+	//If the player dies, handle health
+
+	if (!IsAlive() && !AbilitySystemComponent->HasMatchingGameplayTag(DeadTag))
+	{
+		//Get the Pawn and set dying animation 
+		VehiclePawn->VehicleDie();
+	}
+}
+
+void AVehiclePlayerState::OnMaxHealthChanged(const FOnAttributeChangeData& Data)
+{
+	float MaxHealth = Data.NewValue;
+	UE_LOG(LogTemp, Warning, TEXT("On Player State: OnMaxHealthChanged to %f"), MaxHealth);
+	//Update Pawn Character HUD 
+
+	AVehicleUE5Pawn* VehiclePawn = Cast<AVehicleUE5Pawn>(GetPawn());
+	check(VehiclePawn)
+	if (VehiclePawn && GetLocalRole() != ROLE_Authority)
+	{
+		UVehicleBaseInfoWidget* PlayerInfoWidget = VehiclePawn->GetFloatingStatusBar();
+		check(PlayerInfoWidget)
+		if (PlayerInfoWidget)
+		{
+			PlayerInfoWidget->SetHealthBar(GetHealth() / MaxHealth);
+		}
+	}
+
+}
+
+void AVehiclePlayerState::OnManaChanged(const FOnAttributeChangeData& Data)
+{
+	float Mana = Data.NewValue;
+
+	UE_LOG(LogTemp, Warning, TEXT("On Player State: OnManaChanged to %f"), Mana);
+	
+	//Update Mana on vehicle pawn and UI widgets
+	AVehicleUE5Pawn* VehiclePawn = Cast<AVehicleUE5Pawn>(GetPawn());
+	if (VehiclePawn && GetLocalRole() != ROLE_Authority)
+	{
+		UVehicleBaseInfoWidget* HeroFloatingStatusBar = VehiclePawn->GetFloatingStatusBar();
+		if (HeroFloatingStatusBar)
+		{
+			HeroFloatingStatusBar->SetNitroManaBar(Mana / GetMaxMana());
+		}
+	}
+
+
+}
+
+void AVehiclePlayerState::OnMaxManaChanged(const FOnAttributeChangeData& Data)
+{
+	float MaxMana = Data.NewValue;
+
+	UE_LOG(LogTemp, Warning, TEXT("On Player State: OnMaxManaChanged to %f"), MaxMana);
+
+	//Update Pawn Character HUD 
+
+	AVehicleUE5Pawn* VehiclePawn = Cast<AVehicleUE5Pawn>(GetPawn());
+	if (VehiclePawn && GetLocalRole() != ROLE_Authority)
+	{
+		UVehicleBaseInfoWidget* HeroFloatingStatusBar = VehiclePawn->GetFloatingStatusBar();
+		if (HeroFloatingStatusBar)
+		{
+			HeroFloatingStatusBar->SetNitroManaBar(GetMana() / MaxMana);
+		}
+	}
+}
+
+void AVehiclePlayerState::OnVehicleDamageHit(const FGameplayTag CallbackTag, int32 NewCount)
+{
+
+}
+
--- Source/VehicleUE5/Public/VehiclePlayerState.h
+++ Source/VehicleUE5/Public/VehiclePlayerState.h
@@ -4,6 +4,10 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerState.h"
+#include "AbilitySystemInterface.h"
+#include "GameplayEffectTypes.h"
+#include "Abilities/VehicleAbilitySystemComponent.h"
+#include "Abilities/VehicleAttributeSet.h"
 #include "VehiclePlayerState.generated.h"
 
 /**
@@ -12,15 +16,18 @@
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnCandidateNameChanged, FString, candidateName);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnSetColorIdEvent, int32, colorID);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnSetSessionString, FString , sessionstringID);
 
 
 UCLASS()
-class VEHICLEUE5_API AVehiclePlayerState : public APlayerState
+class VEHICLEUE5_API AVehiclePlayerState : public APlayerState , public IAbilitySystemInterface
 {
 	GENERATED_BODY()
 
 protected:
 	AVehiclePlayerState();
+
+	virtual void BeginPlay() override;
 
 	virtual void ClientInitialize(class AController* InController) override;
 
@@ -41,6 +48,9 @@ protected:
 	UPROPERTY(BlueprintReadWrite, Replicated, ReplicatedUsing = OnRep_TextChat)
 	FString TextChat;
 
+	UPROPERTY(BlueprintReadOnly, Replicated, ReplicatedUsing = OnRep_sessionStringID)
+	FString sessionStringID;
+
 	UFUNCTION()
 	void OnRep_ColorId();
 
@@ -49,13 +59,14 @@ protected:
 
 	UFUNCTION()
 	void OnRep_TextChat();
+
+	UFUNCTION()
+	void OnRep_sessionStringID();
 	
 	UFUNCTION(BlueprintImplementableEvent)
 	void OnReplicatedTextChat(FString const& PlayerName, FString const& Message);
 
 	virtual void PostInitializeComponents() override;
-
-	
 
 
 public:
@@ -68,16 +79,66 @@ public:
 	UPROPERTY(BlueprintAssignable)
 	FOnCandidateNameChanged OnCandidateNameChanged;
 
-
 	UPROPERTY(BlueprintAssignable)
 	FOnSetColorIdEvent OnSetColorEvent;
+
+	UPROPERTY(BlueprintAssignable)
+	FOnSetSessionString OnSetSessionStringEvent;
+
 
 	UFUNCTION()
 	void SetColorId(uint8 colorIndex);
 
+	UFUNCTION()
+	void SetSessionIDString(FString str);
+
 	FORCEINLINE const FString GetCandidateName() const {
 		return CandidateName;
 	}
+
+	class UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+
+	class UVehicleAttributeSet* GetAttributeSetBase() const;
+
+	UFUNCTION(BlueprintCallable, Category = "PlayerState")
+	bool IsAlive() const;
+
+	UFUNCTION(BlueprintCallable, Category = "PlayerState")
+	void ShowAbilityConfirmCancelText(bool ShowText);
+
+	UFUNCTION(BlueprintCallable, Category = "PlayerState")
+	float GetHealth() const;
+
+	UFUNCTION(BlueprintCallable, Category = "PlayerState")
+	float GetMaxHealth() const;
+
+	UFUNCTION(BlueprintCallable, Category = "PlayerState")
+	float GetMana() const;
+
+	UFUNCTION(BlueprintCallable, Category = "PlayerState")
+	float GetMaxMana() const;
+
+
+protected:
+	UPROPERTY()
+	class UVehicleAbilitySystemComponent* AbilitySystemComponent;
+
+	UPROPERTY()
+	class UVehicleAttributeSet* AttributeSetBase;
+
+	FGameplayTag DeadTag;
+
+	FDelegateHandle HealthChangedDelegateHandle;
+	FDelegateHandle MaxHealthChangedDelegateHandle;
+	FDelegateHandle ManaChangedDelegateHandle;
+	FDelegateHandle MaxManaChangedDelegateHandle;
+
+	virtual void OnHealthChanged(const FOnAttributeChangeData& Data);
+	virtual void OnMaxHealthChanged(const FOnAttributeChangeData& Data);
+	virtual void OnManaChanged(const FOnAttributeChangeData& Data);
+	virtual void OnMaxManaChanged(const FOnAttributeChangeData& Data);
+	virtual void OnVehicleDamageHit(const FGameplayTag CallbackTag, int32 NewCount);
+
 
 	
 };
