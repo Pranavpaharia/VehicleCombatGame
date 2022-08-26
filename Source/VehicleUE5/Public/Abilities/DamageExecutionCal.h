7cc25f4863a6e276eb76a5da3823f24744593b38
pranavsharechat <pranavpaharia@sharechat.co>
2022-08-26 05:52:19 +0530

    Added stylized mobile assets pack

---
+++ Source/VehicleUE5/Public/Abilities/DamageExecutionCal.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffectExecutionCalculation.h"
+#include "DamageExecutionCal.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VEHICLEUE5_API UDamageExecutionCal : public UGameplayEffectExecutionCalculation
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/VehicleUE5/Public/Abilities/FireLaserGameplayAbility.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/VehicleGameplayAbility.h"
+#include "FireLaserGameplayAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VEHICLEUE5_API UFireLaserGameplayAbility : public UVehicleGameplayAbility
+{
+	GENERATED_BODY()
+	
+};
