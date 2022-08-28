8f4d7367a4f375731e62e4e331b95c6ca6d48972
pranavsharechat <pranavpaharia@sharechat.co>
2022-08-28 23:46:30 +0530

    Added Collision Profiles Added Physics Settings

---
+++ Source/VehicleUE5/Public/Abilities/ShieldVehicleGameplayAbility.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/VehicleGameplayAbility.h"
+#include "ShieldVehicleGameplayAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VEHICLEUE5_API UShieldVehicleGameplayAbility : public UVehicleGameplayAbility
+{
+	GENERATED_BODY()
+	
+};
