4acdaa1f28c87ec73cf5884c69f76102e18e07c4
pranavsharechat <pranavpaharia@sharechat.co>
2022-08-30 04:34:34 +0530

    Added button widget

--- Source/VehicleUE5/Private/Widgets/ButtonWidget.cpp
+++ Source/VehicleUE5/Private/Widgets/ButtonWidget.cpp
@@ -3,3 +3,27 @@
 
 #include "Widgets/ButtonWidget.h"
 
+void UButtonWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	//if (ButtonObject)
+	//{
+	//	ButtonObject->OnClicked.AddDynamic(this, &UButtonWidget::OnButtonClick);
+	//}
+
+
+	//if (ButtonSoundObject)
+	//{
+	//	//static ConstructorHelpers::FObjectFinder<USoundBase> TempSoundBase(TEXT("SoundWave'/Game/Sounds/Sound_UIBasicClick.Sound_UIBasicClick'"));
+	//	//ButtonSoundObject->SoundClassObject = TempSoundBase.Object;
+	//}
+}
+//
+//void UButtonWidget::OnButtonClick()
+//{
+//	//Add Button Sounds
+//	PlaySound(ButtonSoundObject);
+//
+//	OnButtonClick_BlueprintCaller();
+//}
--- Source/VehicleUE5/Public/Widgets/ButtonWidget.h
+++ Source/VehicleUE5/Public/Widgets/ButtonWidget.h
@@ -4,6 +4,9 @@
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+#include "Components/Button.h"
+#include "Components/TextBlock.h"
+#include "Sound/SoundBase.h"
 #include "ButtonWidget.generated.h"
 
 /**
@@ -13,5 +16,23 @@ UCLASS()
 class VEHICLEUE5_API UButtonWidget : public UUserWidget
 {
 	GENERATED_BODY()
+
+	virtual void NativeConstruct() override;
+	
+	/*public:
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+		UTextBlock* TextObject;
+
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+		UButton* ButtonObject;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (BindWidget))
+		USoundBase* ButtonSoundObject;
+
+	UFUNCTION()
+		void OnButtonClick();
+
+	UFUNCTION(BlueprintImplementableEvent)
+		void OnButtonClick_BlueprintCaller();*/
 	
 };
