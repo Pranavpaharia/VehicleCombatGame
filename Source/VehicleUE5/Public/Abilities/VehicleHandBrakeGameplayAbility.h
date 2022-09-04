c04e840c2d206a190806f9c9fcab457af76101a8
pranavsharechat <pranavpaharia@sharechat.co>
2022-09-04 05:37:40 +0530

    Added Change Vehicle and Handbrake abilities

---
+++ Source/VehicleUE5/Public/Abilities/VehicleHandBrakeGameplayAbility.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/VehicleGameplayAbility.h"
+#include "VehicleHandBrakeGameplayAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VEHICLEUE5_API UVehicleHandBrakeGameplayAbility : public UVehicleGameplayAbility
+{
+	GENERATED_BODY()
+	
+};
