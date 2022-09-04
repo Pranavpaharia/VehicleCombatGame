bcc5ca8d64fb4a5b93ecaf9639ae2097ff306789
pranavsharechat <pranavpaharia@sharechat.co>
2022-09-04 05:42:38 +0530

    added change car vehicle ability

---
+++ Source/VehicleUE5/Private/Abilities/ChangeCarGameplayAbility.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Abilities/ChangeCarGameplayAbility.h"
+#include "VehicleUE5Pawn.h"
+
+UChangeCarGameplayAbility::UChangeCarGameplayAbility()
+{
+	InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
+	NetExecutionPolicy = EGameplayAbilityNetExecutionPolicy::LocalPredicted;
+
+	FGameplayTag AbilityTag = FGameplayTag::RequestGameplayTag(FName("Ability.Skill.ChangeCar"));
+	AbilityTags.AddTag(AbilityTag);
+	ActivationOwnedTags.AddTag(AbilityTag);
+
+	ActivationBlockedTags.AddTag(FGameplayTag::RequestGameplayTag(FName("Ability.Skill")));
+}
+
+void UChangeCarGameplayAbility::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+	if (!CommitAbility(Handle, ActorInfo, ActivationInfo))
+	{
+		EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, true);
+	}
+
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+	UE_LOG(LogTemp, Warning, TEXT("Try Activating Shield  Ability"));
+
+	AVehicleUE5Pawn* VehiclePawn = Cast<AVehicleUE5Pawn>(GetAvatarActorFromActorInfo());
+
+	if (GetOwningActorFromActorInfo()->GetLocalRole() == ROLE_Authority && VehiclePawn)
+	{
+		VehiclePawn->ChangeSkeletalMeshAndAnimBlueprint();
+
+		EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, true);
+	}
+}
